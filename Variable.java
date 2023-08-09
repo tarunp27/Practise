b13678809f15dfcf4bf7b33b09921b65b9fc6a8f
Tarun Pugalendi <104558132+tarunp27@users.noreply.github.com>
2023-08-09 15:05:35 +0530

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,11 @@
+public class Variable {
+    public static void main(String[] args) {
+        int Integer = 24;
+        char String = 'T';
+
+        System.out.println(Integer);
+        System.out.println(String);
+
+        String Runnable = "This is runnbale string";
+    }
+}
